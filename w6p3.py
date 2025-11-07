097d5d564d3c06ccf73bd8e06cb52343d59f58e1
MuhammadjonQilichev-max <muhammadqilichev081@gmail.com>
2025-11-07 18:31:51 +0500

    Add w6p3.py, matrix

---
+++ w6p3.py
@@ -0,0 +1,28 @@
+def sum_diagonals(matrix):
+    n = len(matrix)
+    total = 0
+    
+    for i in range(n):
+        total += matrix[i][i]                   
+        total += matrix[i][n - 1 - i]          
+    if n % 2 == 1:
+        total -= matrix[n // 2][n // 2]
+    
+    return total
+
+matrix_4x4 = [
+    [1,  2,  3,  4],
+    [5,  6,  7,  8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+print(sum_diagonals(matrix_4x4))  # Expected output: 68
+
+
+matrix_3x3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(sum_diagonals(matrix_3x3))  # Expected output: 25
+
